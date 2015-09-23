640452137fb4f906a020d8a90682a36471ea61ce
Bryan Wyatt <brwyatt@gmail.com>
2015-09-22 22:38:07 -0700

    Set up the basics for the big first commit
    Hopefully from this point, furture commits will be smaller, just
    needed to get a good base going. Script should be capable of
    downloading a config file in it's current state, so it's a good
    first start. It can also pick apart short string for GitHub
    references to pick apart a lot of information for the user.

---
+++ functions/downloaderfunctions.sh
@@ -0,0 +1,46 @@
+function download_file {
+  if [ $# == 1 ]; then
+    file=$1
+  else
+    echo "Error: No file provided to download" >&2
+    return 1
+  fi
+
+  if [[ ${file} =~ ^https?:// ]]; then
+    url=${file}
+  elif [[ ${file} =~ ^github: ]]; then
+    if ! url=$(github_parse_path ${file}); then
+      echo "Error parsing ConfigFile as GitHub URL: ${file}" >&2
+      echo "  Function exited with code: $?" >&2
+      return 2
+    fi
+  fi
+
+  if [[ "x${url}" == "x" ]]; then
+    echo "Error: URL is empty string" >&2
+    return 3
+  fi
+
+  dir="/tmp/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w "$((($RANDOM%20)+5))" | head -n1)"
+  if ! mkdir -p "${dir}"; then
+    echo "Could not create tmp directory ${dir}" >&2
+    return 4
+  fi
+  filename=${dir}/${url##*/}
+
+  if which curl >/dev/null; then
+    if curl --silent --output "${filename}" "${url}"; then
+      echo ${filename}
+    else
+      echo "Curl command failed" >&2
+      return 5
+    fi
+  elif which wget >/dev/null; then
+    if wget --quiet -output-document "${filename}" "${url}"; then
+      echo ${filename}
+    else
+      echo "Wget command failed" >&2
+      return 5
+    fi
+  fi
+}
---
+++ functions/githubfunctions.sh
@@ -0,0 +1,26 @@
+export githubraw="https://raw.githubusercontent.com/%user%/%repo%/%tag%/%filepath%"
+export githubarchive="https://github.com/%user%/%repo%/archive/%tag%.tar.gz"
+
+function github_parse_path {
+  if [ $# == 1 ]; then
+    loc=$1
+  else
+    return 1
+  fi
+  if ! [[ ${loc} =~ ^github: ]]; then
+    return 2
+  fi
+
+  user=$([[ ${loc} =~ ^github:([[:alnum:]]*) ]] && echo ${BASH_REMATCH[1]} || return 3)
+  repo=$([[ ${loc} =~ ^github:[[:alnum:]]*/([[:alnum:]]*) ]] && echo ${BASH_REMATCH[1]} || return 3)
+  filepath=$([[ ${loc} =~ ^github:([[:alnum:]]*/){2}([^@]*)(@|$) ]] && echo ${BASH_REMATCH[2]})
+  tag=$([[ ${loc} =~ @(.*)$ ]] && echo ${BASH_REMATCH[1]} || echo "master")
+
+  if [[ "x${filepath}" == "x" ]]; then
+    GitHubURL=$githubarchive
+  else
+    GitHubURL=$githubraw
+  fi
+
+  echo "${GitHubURL}" | sed 's/%user%/'"${user}"'/' | sed 's/%repo%/'"${repo}"'/' | sed 's/%tag%/'"${tag}"'/' | sed 's/%filepath%/'"${filepath/\//\\/}"'/'
+}
